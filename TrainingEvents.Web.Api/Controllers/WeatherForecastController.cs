8d64dab112493c2f2ae5451a4f7e773538b3caf0
Samuel Morgan <>
2020-07-18 17:47:36 +1000

    Added Default Structure/ Simple Refactor of Project Structure

--- TrainingEvents.Web.Api/Controllers/WeatherForecastController.cs
+++ TrainingEvents.Web.Api/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace TrainingEvents.Controllers
+namespace TrainingEvents.Web.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
